3c5e75b55648029a68dd45b96db2ef6e78d8afc4
Süti <suti1979@gmail.com>
2022-06-05 16:53:52 +0200

    signup complete

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -1,32 +1,36 @@
 
 import { useRef, useState } from "react"
 import { Form, Button, Card, Alert } from "react-bootstrap"
-import { Link, useHistory } from "react-router-dom"
+import { useAuth } from "../contexts/AuthContext"
+//import { Link, useHistory } from "react-router-dom"
 
 export default function Signup() {
     const emailRef = useRef()
     const passwordRef = useRef()
     const passwordConfirmRef = useRef()
+    const {signup} = useAuth()
+
+
     const [loading, setLoading] = useState(false)
     const [error, setError] = useState("")
 
     async function handleSubmit(e) {
         e.preventDefault()
     
-        // if (passwordRef.current.value !== passwordConfirmRef.current.value) {
-        //   return setError("Passwords do not match")
-        // }
+        if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+          return setError("Passwords do not match")
+        }
     
-        // try {
-        //   setError("")
-        //   setLoading(true)
-        //   await signup(emailRef.current.value, passwordRef.current.value)
-        //   history.push("/")
-        // } catch {
-        //   setError("Failed to create an account")
-        // }
+        try {
+          setError("")
+          setLoading(true)
+          await signup(emailRef.current.value, passwordRef.current.value)
+          //history.push("/")
+        } catch {
+          setError("Failed to create an account")
+        }
     
-        // setLoading(false)
+        setLoading(false)
       }
 
     return (
@@ -34,6 +38,7 @@ export default function Signup() {
           <Card>
             <Card.Body>
               <h2 className="text-center mb-4">Sign Up</h2>
+          
               {error && <Alert variant="danger">{error}</Alert>}
               <Form onSubmit={handleSubmit}>
                 <Form.Group id="email">
@@ -55,7 +60,7 @@ export default function Signup() {
             </Card.Body>
           </Card>
           <div className="w-100 text-center mt-2">
-            {/* Already have an account? <Link to="/login">Log In</Link> */}
+             Already have an account? {/*<Link to="/login">Log In</Link> */}
           </div>
         </>
       )
