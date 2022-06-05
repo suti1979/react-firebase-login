e30114f4f4cca55262311f2bc60e58ad71addeb4
Süti <suti1979@gmail.com>
2022-06-05 17:28:31 +0200

    login, dashboard

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,19 +1,30 @@
 import Signup from "./Signup"
 import { Container } from "react-bootstrap"
 import { AuthProvider } from "../contexts/AuthContext"
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import Dashboard from "./Dashboard"
+import Login from "./Login"
 
 function App() {
   return (
-    <AuthProvider>
-      <Container
-        className="d-flex align-items-center justify-content-center"
-        style={{ minHeight: "100vh" }}
-      >
-        <div className="w-100" style={{ maxWidth: "400px" }}>
-          <Signup />
-        </div>
-      </Container>
-    </AuthProvider>
+    <Container
+      className="d-flex align-items-center justify-content-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <div className="w-100" style={{ maxWidth: "400px" }}>
+        <Router>
+          <AuthProvider>
+            <Routes>
+              <Route exact path="/" element={<Dashboard/>} />
+              {/* <PrivateRoute path="/update-profile" component={UpdateProfile} /> */}
+              <Route path="/signup" element={<Signup/>} />
+              <Route path="/login" element={<Login/>} />
+              {/* <Route path="/forgot-password" component={ForgotPassword} /> */}
+            </Routes>
+          </AuthProvider>
+        </Router>
+      </div>
+    </Container>
   )
 }
 
--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,5 +1,5 @@
 import { createContext, useContext, useEffect, useState } from "react"
-import {auth} from "../firebase"
+import { auth } from "../firebase"
 
 const AuthContext = createContext()
 
@@ -8,29 +8,55 @@ export function useAuth() {
 }
 
 export function AuthProvider({ children }) {
-  const [currentUser, setCurrentUser] = useState()
-  const [loading, setLoading] = useState(true)
-
-  function signup(email, password) {
+    const [currentUser, setCurrentUser] = useState()
+    const [loading, setLoading] = useState(true)
+  
+    function signup(email, password) {
       return auth.createUserWithEmailAndPassword(email, password)
-  }
-
-  useEffect(() => {
+    }
+  
+    function login(email, password) {
+      return auth.signInWithEmailAndPassword(email, password)
+    }
+  
+    function logout() {
+      return auth.signOut()
+    }
+  
+    function resetPassword(email) {
+      return auth.sendPasswordResetEmail(email)
+    }
+  
+    function updateEmail(email) {
+      return currentUser.updateEmail(email)
+    }
+  
+    function updatePassword(password) {
+      return currentUser.updatePassword(password)
+    }
+  
+    useEffect(() => {
       const unsubscribe = auth.onAuthStateChanged(user => {
-          setCurrentUser(user)
-          setLoading(false)
+        setCurrentUser(user)
+        setLoading(false)
       })
+  
       return unsubscribe
-  }, [])
-
-
-
-  const value = {
+    }, [])
+  
+    const value = {
       currentUser,
-      signup
+      login,
+      signup,
+      logout,
+      resetPassword,
+      updateEmail,
+      updatePassword
+    }
+  
+    return (
+      <AuthContext.Provider value={value}>
+        {!loading && children}
+      </AuthContext.Provider>
+    )
   }
-
-  return <AuthContext.Provider value={value}>
-            {!loading && children}
-         </AuthContext.Provider>
-}
